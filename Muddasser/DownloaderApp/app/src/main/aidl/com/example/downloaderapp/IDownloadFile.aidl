dd927d461b9938be653ab9377924e775277748c3
Jerome John <33810944+johnjerome1992@users.noreply.github.com>
2019-05-05 15:10:29 +0200

    chg

--- Muddasser/DownloaderApp/app/src/main/aidl/com/example/downloaderapp/IDownloadFile.aidl
+++ Muddasser/DownloaderApp/app/src/main/aidl/com/example/downloaderapp/IDownloadFile.aidl
@@ -8,5 +8,5 @@ interface IDownloadFile {
      * Demonstrates some basic types that you can use as parameters
      * and return values in AIDL.
      */
-    void downloadFile( String url);
+    int downloadFile( String url);
 }
--- Muddasser/DownloaderApp/app/src/main/java/com/example/downloaderapp/DownloadFileService.java
+++ Muddasser/DownloaderApp/app/src/main/java/com/example/downloaderapp/DownloadFileService.java
@@ -1,4 +1,41 @@
 package com.example.downloaderapp;
 
-public class DownloadFileService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class DownloadFileService extends Service{
+
+    private DownloadFileServiceImpl impl;
+
+    private class DownloadFileServiceImpl extends IDownloadFile.Stub {
+
+        @Override
+        public int downloadFile(String Str) {
+            return 0;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        impl = new DownloadFileServiceImpl();
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent){
+        return impl;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent,int flags,int startId){
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
 }
