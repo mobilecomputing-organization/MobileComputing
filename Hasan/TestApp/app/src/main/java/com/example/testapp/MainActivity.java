269d16a5bd50fa694bc94c5fc11ffb16e4693fbe
Jerome John <33810944+johnjerome1992@users.noreply.github.com>
2019-05-20 01:08:53 +0200

    Temperature and Humiditz Working for Read and Fan control done
    Notifz pending

--- Hasan/TestApp/app/src/main/java/com/example/testapp/MainActivity.java
+++ Hasan/TestApp/app/src/main/java/com/example/testapp/MainActivity.java
@@ -19,6 +19,7 @@ import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.UUID;
@@ -28,8 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothLeScanner LeScanner;
     private BluetoothDevice MyDevice;
-    private ScanCallback scanCallback;
-    private BluetoothGattCallback GattCallback;
+    // private ScanCallback scanCallback;
+    // private BluetoothGattCallback GattCallback;
     private BluetoothGatt mygatt = null;
     private Handler mHandler;
 
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean IsScanSuccess = false;
     private UUID fanServiceuuid = UUID.fromString("00000001-0000-0000-FDFD-FDFDFDFDFDFD");
     private UUID Fan_Intensity=  UUID.fromString("10000001-0000-0000-FDFD-FDFDFDFDFDFD");
+    public boolean bReadTemprature = false;
+    public String strHumidityVal = "N/A";
+    public String strTempVal = "N/A";
 
     private UUID WeatherServiceUUID = UUID.fromString("00000002-0000-0000-FDFD-FDFDFDFDFDFD");
     private UUID Temperature_Measurement =  UUID.fromString("00002a1c-0000-1000-8000-00805f9b34fb")  ;
@@ -51,7 +55,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button LeftButton;
     private Button RightButton;
     private Button DownButton;
-    private TextView DispText;
+    public TextView DispText;
+    private EditText FanIntensity;
+    private TextView FanIntensityText;
 
     private enum DisplayStates{
         INIT,
@@ -61,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         READ_ALL,
         SCANNING,
         WEATHER,
-    //    TEMPERATURE_READ,
+        //    TEMPERATURE_READ,
         FAN_Write
     }
     private enum ButtonStates {
@@ -82,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ButtonStates DownButtonState = ButtonStates.CONNECT;
 //    private DisplayStates CurrentState = DisplayStates.INIT;
 
-//=====================================//
+    //=====================================//
     // SOME LOCAL FUNCTIONS
 //=====================================//
     public void StartScanning(boolean IsWeatherSelected){
@@ -103,7 +109,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         }, SCAN_PERIOD);
-
 
         LeScanner.startScan(scanCallback);
 
@@ -128,6 +133,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 LeftButton.setVisibility(View.VISIBLE);
                 RightButton.setVisibility(View.VISIBLE);
+                FanIntensity.setVisibility(View.INVISIBLE);
+                FanIntensityText.setVisibility(View.INVISIBLE);
+
                 DispText.setText("");
                 DownButton.setText("Connect");
                 LeftButton.setText("SCAN TEMP");
@@ -163,6 +171,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 LeftButton.setText("Write");
                 RightButton.setVisibility(View.INVISIBLE);
+                FanIntensity.setVisibility(View.VISIBLE);
+                FanIntensityText.setVisibility(View.VISIBLE);
                 DownButton.setText("Disconnect");
 
                 break;
@@ -171,23 +181,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 RightButtonState = ButtonStates.READ_HUMIDITY;
                 DownButtonState = ButtonStates.DISCONNECT;
 
-                LeftButton.setText("T");
-                RightButton.setText("H");
+                LeftButton.setText("T_Update");
+                RightButton.setText("H_Update");
                 RightButton.setVisibility(View.VISIBLE);
                 LeftButton.setVisibility(View.VISIBLE);
                 DownButton.setText("Disconnect");
                 break;
             case WEATHER:
                 LeftButtonState = ButtonStates.READ_WEATHER;
+                RightButtonState = ButtonStates.NOTIFY_WEATHER;
                 DownButtonState = ButtonStates.DISCONNECT;
 
                 LeftButton.setText("Read");
+                RightButton.setText("Notify");
                 LeftButton.setVisibility(View.VISIBLE);
-                RightButton.setVisibility(View.INVISIBLE);
+                RightButton.setVisibility(View.VISIBLE);
                 DownButton.setText("Disconnect");
 
                 break;
         }
+    }
+
+    public void setText(final TextView text,final String value){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.setText(value);
+            }
+        });
     }
 
 ///=================================///
@@ -203,12 +224,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         RightButton = findViewById(R.id.RightButton);
         DownButton = findViewById(R.id.DownButton);
         DispText = findViewById(R.id.Disp);
+        FanIntensity = findViewById(R.id.FanIntensity);
+        FanIntensityText = findViewById(R.id.FanIntensityText);
 
         LeftButton.setOnClickListener(this);
         RightButton.setOnClickListener(this);
         DownButton.setOnClickListener(this);
 
         DownButton.setVisibility(View.INVISIBLE);
+        FanIntensity.setVisibility(View.INVISIBLE);
+        FanIntensityText.setVisibility(View.INVISIBLE);
 
         mHandler = new Handler();
 
@@ -222,90 +247,168 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+    }
 
-        //-------------------------------------//
-        // Device scan callback.
-        //-------------------------------------//
-        scanCallback = new ScanCallback() {
-            @Override
-            public void onScanResult(int callbackType, ScanResult result) {
+    ///=================================///
+    // ON CLICK STUFF //
+///=================================///
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.LeftButton:
+                switch (LeftButtonState) {
+                    case SCAN_WEATHER:
+                        StartScanning(true);
+                        break;
+                    case WRITE_FAN:
+                        mygatt= MyDevice.connectGatt(MainActivity.this,false,GattCallback);
+                        break;
+                    case READ_WEATHER:
+                        //mygatt= MyDevice.connectGatt(MainActivity.this,false,GattCallback);
+                        DispGUI(DisplayStates.READ_ALL);
+                        break;
+                    case READ_TEMPRATURE:
+                        bReadTemprature = true;
+                        mygatt= MyDevice.connectGatt(MainActivity.this,false,GattCallback);
+                        //mygatt.readCharacteristic(Temp_characteristic);
+                        break;
+                }
+                break;
+            case R.id.RightButton:
+                //TODO check if correct
+                switch (RightButtonState) {
+                    case SCAN_FAN:
+                        StartScanning(false);
+                        break;
+                    case READ_HUMIDITY:
+                        bReadTemprature = false;
+                        mygatt= MyDevice.connectGatt(MainActivity.this,false,GattCallback);
+                        //mygatt.readCharacteristic(Temp_characteristic);
+                        break;
+                }
+                break;
+            case R.id.DownButton:
+                switch (DownButtonState) {
+                    case CONNECT:
+                        if (appname == "IPVS-LIGHT") {
+                            DispGUI(DisplayStates.FAN_Write);
+                        } else {
+                            DispGUI(DisplayStates.WEATHER);
+                        }
+                        break;
+                    case DISCONNECT:
+                        //mygatt.disconnect();
+                        DispGUI(DisplayStates.INIT);
+                        break;
+                }
+                break;
+        }
+    }
+///=================================///
+    // THE END //
+///=================================///
+
+    //-------------------------------------//
+    // Device scan callback.
+    //-------------------------------------//
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
 
             //    Log.i(TAG, "Called");
 
-                if (result.getScanRecord() != null) {
+            if (result.getScanRecord() != null) {
 
                 //    Log.i(TAG, "inside If");
 
-                    if (result.getScanRecord().getDeviceName() != null) {
+                if (result.getScanRecord().getDeviceName() != null) {
 
-                        if (result.getScanRecord().getDeviceName().equals(appname)) {
+                    if (result.getScanRecord().getDeviceName().equals(appname)) {
 
-                            // Stop the Scan becasue the wanted device is found
+                        // Stop the Scan becasue the wanted device is found
 
-                            LeScanner.stopScan(scanCallback);
-                            DispGUI(DisplayStates.DEVICE_FOUND);
-                            IsScanSuccess = true;
+                        LeScanner.stopScan(scanCallback);
+                        DispGUI(DisplayStates.DEVICE_FOUND);
+                        IsScanSuccess = true;
 
-                            //Store the found device in a local Blt Device
-                            MyDevice = result.getDevice();
+                        //Store the found device in a local Blt Device
+                        MyDevice = result.getDevice();
 
-                        } else {
+                    } else {
                         //    DispText.setText("Could not find device");
-                        }
                     }
                 }
             }
-            };
-        //----------------------//
-        //GATT CALLBACK
-        //----------------------//
-        GattCallback = new BluetoothGattCallback() {
-            @Override
-            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-                Log.i(TAG, " onConnectionStateChange: " + newState );
-                if (newState == BluetoothProfile.STATE_CONNECTED){
-                    Log.i(TAG, " onConnectionStateChange: " + status );
-                    DispText.setText("GATT!!");
-                    //ScanRes.setText(ScanRes.getText()+ "state_connected wait for service discovery \n");
-                    gatt.discoverServices();
-                }
+        }
+    };
+
+    //----------------------//
+    //GATT CALLBACK
+    //----------------------//
+
+    private BluetoothGattCallback GattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            Log.i(TAG, " onConnectionStateChange: " + newState );
+            if (newState == BluetoothProfile.STATE_CONNECTED){
+                Log.i(TAG, " onConnectionStateChange: " + status );
+                //DispText.setText("GATT!!");
+                setText(DispText,"Server Connected \n");
+                //ScanRes.setText(ScanRes.getText()+ "state_connected wait for service discovery \n");
+                gatt.discoverServices();
             }
+        }
 
-            @Override
-            public void onServicesDiscovered(BluetoothGatt gatt, int status){
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status){
 
-                if (status == BluetoothGatt.GATT_SUCCESS) {
-//                    //ScanRes.setText(ScanRes.getText()+ "service discovered  "+ status +"\n");
-                    Log.i(TAG, "onServicesDiscovered received1: " + status );
-//
-                    if (gatt.getService(WeatherServiceUUID) != null )
-                    {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                    //ScanRes.setText(ScanRes.getText()+ "service discovered  "+ status +"\n");
+                Log.i(TAG, "onServicesDiscovered received1: " + status );
+                setText(DispText,"");
 
+                if (gatt.getService(WeatherServiceUUID) != null )
+                {
+
+                    if (bReadTemprature) {
                         Temp_characteristic =  gatt.getService(WeatherServiceUUID).getCharacteristic(Temperature_Measurement);
-                        Humidity_characteristic =  gatt.getService(WeatherServiceUUID).getCharacteristic(Humidity_Measurement);
+                        mygatt.readCharacteristic(Temp_characteristic);
 
-                        //                        if(isNotify == true)
+                    } else {
+                        Humidity_characteristic =  gatt.getService(WeatherServiceUUID).getCharacteristic(Humidity_Measurement);
+                        mygatt.readCharacteristic(Humidity_characteristic);
+                    }
+
+
+                    //                        if(isNotify == true)
 //                        {
 //                            gatt.setCharacteristicNotification(characteristic1, true);
 //                            //gatt.setCharacteristicNotification(characteristic2, true);
 //                        }
 
-                    }
-                    else if (gatt.getService(fanServiceuuid) != null){
-                        BluetoothGattCharacteristic characteristic =  gatt.getService(fanServiceuuid).getCharacteristic(Fan_Intensity);
-                        characteristic.setValue(0,BluetoothGattCharacteristic.FORMAT_UINT16,0);
-                        gatt.writeCharacteristic(characteristic);
-                    }
                 }
+                else if (gatt.getService(fanServiceuuid) != null){
+                    BluetoothGattCharacteristic characteristic =  gatt.getService(fanServiceuuid).getCharacteristic(Fan_Intensity);
+                  //  characteristic.setValue(0,BluetoothGattCharacteristic.FORMAT_UINT16,0);
+                    Log.i(TAG, "FanIntensity test : " + FanIntensity.getText());
+                    Log.i(TAG, "FanIntensity test : " + Integer.parseInt(FanIntensity.getText().toString()));
+
+                    characteristic.setValue(Integer.parseInt(FanIntensity.getText().toString()),BluetoothGattCharacteristic.FORMAT_UINT16,0);
+
+                    gatt.writeCharacteristic(characteristic);
+                }
+
+
+            }
 //                else {
 //                    Log.i(TAG, "onServicesDiscovered received2: " + status );
 //                }
 //                //BluetoothGattService sercharatersitic =  gatt.getService(UUID.fromString("0000fff1-0000-1000-8000-00805f9b34fb"));
 //                //Temperaturedata.setText(Temperaturedata.getText()+ "character   " + characteristic.getService().getUuid().toString());
-            }
+        }
 
-            @Override
-            public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
 //                super.onCharacteristicRead(gatt, characteristic, status);
 //                //ScanRes.setText(ScanRes.getText()+ "\n readchar   " + characteristic.getValue());
 //                Log.i(TAG, "onCharacteristicRead outside IF : " + status + characteristic.getUuid());
@@ -323,71 +426,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                else {
 //                    // do nothing
 //                }
-                DispText.setText(characteristic.getValue().toString());
-                mygatt.disconnect();
+
+            if (bReadTemprature)
+            {
+                int temperature_data = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_SINT16,1);
+                strTempVal = String.valueOf ((float)(temperature_data/100));
+                setText(DispText, strTempVal + "°C "+strHumidityVal + "% \n");
+            }else
+            {
+                float percentage = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT16,0)*(float)(0.01);
+                strHumidityVal = Float.toString(percentage);;
+                setText(DispText, strTempVal + "°C "+strHumidityVal + "% \n");
             }
 
-            @Override
-            public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+
+
+
+            mygatt.disconnect();
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
             //    super.onCharacteristicWrite(gatt, characteristic, status);
-                mygatt.disconnect();
-            }
+
+            mygatt.disconnect();
+        }
 
         //    @Override
         //    public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-              //  gatt.readCharacteristic(characteristic);
-                //ScanRes.setText(ScanRes.getText()+ "get value   " + characteristic.getValue());
-            //}
+        //  gatt.readCharacteristic(characteristic);
+        //ScanRes.setText(ScanRes.getText()+ "get value   " + characteristic.getValue());
+        //}
 
-        };
-        }
+    };
 
-///=================================///
-    // ON CLICK STUFF //
-///=================================///
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.LeftButton:
-                switch (LeftButtonState) {
-                    case SCAN_WEATHER:
-                        StartScanning(true);
-                        break;
-                    case WRITE_FAN:
-                        mygatt= MyDevice.connectGatt(v.getContext(),false,GattCallback);
-                        break;
-                    case READ_WEATHER:
-                        mygatt= MyDevice.connectGatt(v.getContext(),false,GattCallback);
-                        DispGUI(DisplayStates.READ_ALL);
-                        break;
-                    case READ_TEMPRATURE:
-                        mygatt.readCharacteristic(Temp_characteristic);
-                        break;
-                }
-                break;
-            case R.id.RightButton:
-                //TODO// ADD FUNCTIONALITY
-                StartScanning(false);
 
-                break;
-            case R.id.DownButton:
-                switch (DownButtonState) {
-                    case CONNECT:
-                        if (appname == "IPVS-LIGHT") {
-                            DispGUI(DisplayStates.FAN_Write);
-                        } else {
-                            DispGUI(DisplayStates.WEATHER);
-                        }
-                        break;
-                    case DISCONNECT:
-                            //mygatt.disconnect();
-                            DispGUI(DisplayStates.INIT);
-                        break;
-                }
-                break;
-        }
-    }
-///=================================///
-    // THE END //
-///=================================///
+
 }
