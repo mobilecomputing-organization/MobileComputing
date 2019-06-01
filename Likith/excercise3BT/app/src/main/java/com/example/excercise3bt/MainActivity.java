9aa5cb819ff300aa55ed4a1edf68cb915919624d
Hasan6979 <50082256+Hasan6979@users.noreply.github.com>
2019-06-01 21:11:49 +0200

    Some good changes in file
    Print the received advertisment

--- Likith/excercise3BT/app/src/main/java/com/example/excercise3bt/MainActivity.java
+++ Likith/excercise3BT/app/src/main/java/com/example/excercise3bt/MainActivity.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
@@ -11,6 +12,9 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewDebug;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -19,15 +23,25 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothLeScanner LeScanner;
     private Handler handler;
+
     private final int REQUEST_ENABLE_BT = 1234; // just to pass as an argument
     private final int SCAN_PERIOD = 10000;// 10 seconds
-    private String appname;
+
+    private String appname = ""; //TODO FILL THE APP NAME //
     private TextView beaconText;
+    private Button StartButton;
+
+//    private int RcvdRssi;
+    private ScanRecord scanRecord;
+    private byte[] RecvdMsg;
+//    private int RcvdTxPower;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StartButton = findViewById(R.id.StartButton);
         beaconText = findViewById(R.id.beaconText);
         handler = new Handler();
 
@@ -41,7 +55,14 @@ public class MainActivity extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
-        startScanning();
+        StartButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                startScanning();
+            }
+        }
+        );
     }
 
     private void startScanning() {
@@ -57,6 +78,24 @@ public class MainActivity extends AppCompatActivity {
         LeScanner.startScan(scanCallback);
     }
 
+    public static String ByteArrayToString(byte[] ba)
+    {
+        StringBuilder hex = new StringBuilder(ba.length * 2);
+        for (byte b : ba)
+            hex.append(b + " ");
+
+        return hex.toString();
+    }
+
+    public void printScanRecord (byte[] scanRecord) {
+        // Simply print all raw bytes
+    //    String decodedRecord = new String(scanRecord,"UTF-8");
+        Log.i(TAG,"decoded String : " + ByteArrayToString(scanRecord));
+    }
+
+    //-------------------------------------//
+    // Device scan callback.
+    //-------------------------------------//
     private ScanCallback scanCallback = new ScanCallback() {
         //startScan(List<ScanFilter> filters, ScanSettings settings, PendingIntent callbackIntent) if needed
         @Override
@@ -71,11 +110,10 @@ public class MainActivity extends AppCompatActivity {
                 if (result.getScanRecord().getDeviceName() != null) {
                     Log.i(TAG, result.getScanRecord().getDeviceName());
                     if (result.getScanRecord().getDeviceName().equals(appname)) {
-                        LeScanner.stopScan(scanCallback);
+                     //   LeScanner.stopScan(scanCallback);
 
                         //check or mask is needed to check for the kind of advertisement
                         // UID, URL, TLM
-
 
                         /*
                         data to be displayed
@@ -84,11 +122,13 @@ public class MainActivity extends AppCompatActivity {
                         ◦ Voltage
                         ◦ Temperature
                         ◦ Distance to beacon in meter (optimize estimation/calibration)*/
-                        result.getRssi();//Returns the received signal strength in dBm.
-                        result.getScanRecord();//advertisement data
-                        result.getTxPower();//Returns the transmit power in dBm.
+                    //    RcvdRssi = result.getRssi();//Returns the received signal strength in dBm.
+                        scanRecord = result.getScanRecord();//advertisement data
+                        RecvdMsg = scanRecord.getBytes();
+                        printScanRecord(RecvdMsg);
+                    //    RcvdTxPower = result.getTxPower();//Returns the transmit power in dBm.
 
-                        beaconText.setText("result");
+                    //    beaconText.setText("RSSI: " + RcvdRssi + "\nTx Power: " + RcvdTxPower);
                     }
                 }
             }
