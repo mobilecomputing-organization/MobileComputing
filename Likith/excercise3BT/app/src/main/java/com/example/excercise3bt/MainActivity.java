ad3afd4017e20ecaf9c75a319a85c015a5e70a10
Hasan6979 <50082256+Hasan6979@users.noreply.github.com>
2019-06-02 18:55:20 +0200

    Almost Done. Some TODOs

--- Likith/excercise3BT/app/src/main/java/com/example/excercise3bt/MainActivity.java
+++ Likith/excercise3BT/app/src/main/java/com/example/excercise3bt/MainActivity.java
@@ -13,9 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewDebug;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler;
 
     private final int REQUEST_ENABLE_BT = 1234; // just to pass as an argument
-    private final int SCAN_PERIOD = 10000;// 10 seconds
+    private final int SCAN_PERIOD = 20000;// 10 seconds
 
     private String appname = ""; //TODO FILL THE APP NAME //
     private TextView beaconText;
@@ -34,7 +35,15 @@ public class MainActivity extends AppCompatActivity {
 //    private int RcvdRssi;
     private ScanRecord scanRecord;
     private byte[] RecvdMsg;
-//    private int RcvdTxPower;
+    private static final byte UID_PACKET = 0;
+    private static final byte URL_PACKET = 16;
+    private static final byte TLM_PACKET = 32;
+    private static final byte HTTP_WWW_FRAME = 0;
+    private static final byte HTTPS_WWW_FRAME = 1;
+    private static final byte HTTP_OHNE_WWW_FRAME = 2;
+    private static final byte HTTPS_0HNE_WWW_FRAME = 3;
+
+    //    private int RcvdTxPower;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +87,58 @@ public class MainActivity extends AppCompatActivity {
         LeScanner.startScan(scanCallback);
     }
 
+    public void parseMsg(byte[] BuffData)
+    {
+        String print_Str = "";
+        byte[] UsefulData = {0};
+        String Data_str = "";
+        // TODO Change 0x56 to FF AND AE
+        Log.i(TAG, BuffData[2] + "AND" + BuffData[3]);
+        if (BuffData[2] == -86 && BuffData[3] == -2) // Packet is of EddyStone
+        {
+            switch(BuffData[8]) {
+                case UID_PACKET:
+                    Log.i(TAG, "A WASP");
+                    //TODO Display as HEX instead of INTS
+                    UsefulData = Arrays.copyOfRange(BuffData, 10, 20);
+                    Data_str = ByteArrayToString(UsefulData);
+                    beaconText.setText(Data_str);
+                    break;
+                case URL_PACKET:
+                    switch (BuffData[10]) {
+                        case HTTP_WWW_FRAME:
+                            print_Str = "http://www.";
+                            break;
+                        case HTTPS_WWW_FRAME:
+                            print_Str = "https://www.";
+                            break;
+                        case HTTP_OHNE_WWW_FRAME:
+                            print_Str = "http://";
+                            break;
+                        case HTTPS_0HNE_WWW_FRAME:
+                            print_Str = "https://";
+                            break;
+                    }
+                    UsefulData = Arrays.copyOfRange(BuffData, 11, BuffData.length);
+                    try {
+                        Data_str = new String(UsefulData, "UTF-8");
+                    } catch (Exception UnsupportedEncodingException)
+                    {
+                        // Do Nothing
+                    }
+
+                    print_Str = print_Str + Data_str;
+                    beaconText.setText(print_Str);
+                    break;
+                case TLM_PACKET:
+                    // TODO Parse This Packet
+                    Log.i(TAG, "A Spider");
+                    break;
+            }
+
+        }
+    }
+
     public static String ByteArrayToString(byte[] ba)
     {
         StringBuilder hex = new StringBuilder(ba.length * 2);
@@ -104,13 +165,16 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, "scanCallback ...");
 
             if (result.getScanRecord() != null) {
-                Log.i(TAG, result.getScanRecord().toString());
+                //Log.i(TAG, result.getScanRecord().toString());
+                Log.i(TAG, "FLY on the wall..");
                 //simulator result
                 // ScanRecord [mAdvertiseFlags=26, mServiceUuids=null, mManufacturerSpecificData={76=[16, 5, 3, 24, -59, -88, -81]}, mServiceData={}, mTxPowerLevel=-2147483648, mDeviceName=null]
-                if (result.getScanRecord().getDeviceName() != null) {
-                    Log.i(TAG, result.getScanRecord().getDeviceName());
-                    if (result.getScanRecord().getDeviceName().equals(appname)) {
-                     //   LeScanner.stopScan(scanCallback);
+                if (result.getDevice().getAddress() != null) {
+                    Log.i(TAG, "Woh an INSECT!");
+                    Log.i(TAG, result.getDevice().getAddress());
+                    //    Log.i(TAG, result.getScanRecord().getDeviceName());
+                //    if (result.getScanRecord().getDeviceName().equals(appname)) {
+                    // TODO ADD IF-STATEMENT OF MAC ADDRESS
 
                         //check or mask is needed to check for the kind of advertisement
                         // UID, URL, TLM
@@ -125,11 +189,12 @@ public class MainActivity extends AppCompatActivity {
                     //    RcvdRssi = result.getRssi();//Returns the received signal strength in dBm.
                         scanRecord = result.getScanRecord();//advertisement data
                         RecvdMsg = scanRecord.getBytes();
-                        printScanRecord(RecvdMsg);
+                        parseMsg(RecvdMsg);
+                    //    printScanRecord(RecvdMsg);
                     //    RcvdTxPower = result.getTxPower();//Returns the transmit power in dBm.
 
                     //    beaconText.setText("RSSI: " + RcvdRssi + "\nTx Power: " + RcvdTxPower);
-                    }
+                //    }
                 }
             }
         }
