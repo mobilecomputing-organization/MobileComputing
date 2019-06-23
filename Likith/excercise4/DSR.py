22b3b3e19e3ff75c9c1f49d8140d3d049b7f4eb2
likhith-bp <49592284+likhith-bp@users.noreply.github.com>
2019-06-23 15:26:14 +0200

    DSR
    dsr

--- Likith/excercise4/DSR.py
+++ Likith/excercise4/DSR.py
@@ -1,9 +1,13 @@
 import socket
 import time
 import commands
+import sys
 
+
+print "HELLoo!!!!",str(sys.argv)
 UDP_IP = "192.168.210.255"
 IP = commands.getoutput("ifconfig wlan0 | grep -Po  'inet \K[\d.]+'")
+node = IP[-3:]
 UDP_PORT = 5012
 print " IP: ", IP
 
@@ -18,14 +22,19 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind((UDP_IP, UDP_PORT))
 sock.settimeout(60)
 
+if len(sys.argv) is  2:
+	orig_seqNumber = sys.argv[1]
+	sock.sendto(sys.argv[1], (UDP_IP, UDP_PORT))
+	
+
 while time.time() < t_end:
 
     try:
         data,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         rcvd_seqNumber = data
         if addr is not None:
-            print  "RX( ", addr, IP, " ) ", "Latency", " RX data: ", data
-            addr = None
+			if addr[0] is not IP 
+				print  "RX( ", addr, IP, " ) ", "Latency", " RX data: ", data
         
             if (rcvd_seqNumber != orig_seqNumber) or (orig_seqNumber == -1) :
                 orig_seqNumber = rcvd_seqNumber
@@ -33,11 +42,7 @@ while time.time() < t_end:
                 sock.sendto(orig_seqNumber, (UDP_IP, UDP_PORT))
 
     except socket.error:
-        addr = None
         pass
 
 sock.close()
 print "finished"
-
-
-
