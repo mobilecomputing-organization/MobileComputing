e88329501c00e9d8f041bd43794d88c2d4ca2e24
likhith-bp <49592284+likhith-bp@users.noreply.github.com>
2019-06-23 21:55:31 +0200

    Update DSR.py

--- Likith/excercise4/DSR.py
+++ Likith/excercise4/DSR.py
@@ -3,8 +3,6 @@ import time
 import commands
 import sys
 
-
-print "HELLoo!!!!",str(sys.argv)
 UDP_IP = "192.168.210.255"
 IP = commands.getoutput("ifconfig wlan0 | grep -Po  'inet \K[\d.]+'")
 node = IP[-3:]
@@ -18,25 +16,33 @@ addr = None
 RREP="1"
 RREQ="0"
 RList=[]
+msg=" "
+msg2=" "
+
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind((UDP_IP, UDP_PORT))
 sock.settimeout(40)
 
-if len(sys.argv) is  2:
+if len(sys.argv) >=  2:
 	orig_seqNumber = sys.argv[1]
-	data = RREQ+":"+node
-	sock.sendto(data, (UDP_IP, UDP_PORT))
-	
+	if len(sys.argv) is  3:
+                msg = sys.argv[2]
+
+        data = RREQ+":"+node+"#"+msg
+        print   " data: ", data 
+        sock.sendto(data, (UDP_IP, UDP_PORT))
 
 while time.time() < t_end:
 
     try:
-        data,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        
+        fulldata,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        data = fulldata.split("#",1)[0]
+        #msg2 = fulldata.split("#",1)[1]
+        #data[0:123.12.12.33#hello word]
         if addr is not None and addr[0] != IP:
-                print  "from: ", addr[0], " data: ", data
+                print  "from: ", addr[0], " data: ", data , "msgRecv",msg2
                 if data[0] == RREQ:
                         if node not in data:
                                 s1 = data+":"+node
@@ -51,19 +57,18 @@ while time.time() < t_end:
                                 addr = None
                 else:
                         if node in data:
-                                print "we have node"
+                                #print "we have node"
                                 if node == data[2:5]:
-                                        RList.append(data[2:])
-                                        print "path to ",data[-3:]," is ",data[2:]
+                                        if data[2:] not in RList:
+                                                RList.append(data[2:])
+                                                print "path to ",data[-3:]," is ",data[2:]
                                 else:
                                         idx = data.find(node)
                                         s= UDP_IP[:-3]+data[idx-4:idx-1]
                                         print "addr[0]",addr[0],"return: ",s ," data:" ,data
                                         #sock.sendto(data, (UDP_IP[:-3]+data[idx-4:idx-1], UDP_PORT))
 
-                                        if data[2:] in RList:
-                                                print "!!!!!"
-                                        else:
+                                        if data[2:] not in RList:
                                                 sock.sendto(data, (UDP_IP, UDP_PORT))
                                                 RList.append(data[2:])
                                 addr = None
