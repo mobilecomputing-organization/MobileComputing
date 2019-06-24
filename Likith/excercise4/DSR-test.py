1290eb70cb4549b7abaebe4cbd3dff8efd1b5992
likhith-bp <49592284+likhith-bp@users.noreply.github.com>
2019-06-24 22:18:27 +0200

    working with data format bitForREQ:path#data#time
    working with data format bitForREQ:path#data#time

--- Likith/excercise4/DSR-test.py
+++ Likith/excercise4/DSR-test.py
@@ -16,8 +16,8 @@ addr = None
 RREP="1"
 RREQ="0"
 RList=[]
-msg=" "
-msg2=" "
+msg="msg"
+msg2="msg2"
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -28,33 +28,41 @@ startTime = time.time()
 
 if len(sys.argv) >=  2:
 	orig_seqNumber = sys.argv[1]
-	if len(sys.argv) is  3:
+	if len(sys.argv) >  3:
                 msg = sys.argv[2]
         startTime = time.time()
-        data = RREQ+":"+node+"#"+msg
-        print   " data: ", data 
+        data = RREQ+":"+node+"#"+msg+"#"+str(startTime)
+        print   " data: ", data
         sock.sendto(data, (UDP_IP, UDP_PORT))
 
 while time.time() < t_end:
 
     try:
         fulldata,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        data = fulldata.split("#",2)[0]
-        msg2 = fulldata.split("#",2)[1]
-        startTime = fulldata.split("#",2)[2]
+        fdata=fulldata.split("#",1)
+	print "fulldata: ",fulldata
+	print "fdata: ",fdata
+	data = fdata[0]
+        forwardData = fdata[1]
+	msgData = forwardData .split("#",1)
+	msg2 = msgData [0]
+        startTime = msgData [1]
         #data[0:123.12.12.33#hello word#125445545time stamp in ms]
         if addr is not None and addr[0] != IP:
                 print  "from: ", addr[0], " data: ", data , "msgRecv",msg2
                 if data[0] == RREQ:
                         if node not in data:
-                                s1 = data+":"+node
-                                #print "send s1 " ,s1
-                                sock.sendto(data+":"+node, (UDP_IP, UDP_PORT))
+                                sendReq = data+":"+node+"#"+forwardData
+                                print "sendReq  " ,sendReq 
+                                sock.sendto(sendReq  , (UDP_IP, UDP_PORT))
                                 #time.sleep(1)
-                                
+
                                 s2 = RREP+data[1:]+":"+node
                                 ss= UDP_IP[:-3]+data[-3:]
-                                sock.sendto(RREP+data[1:]+":"+node+"#"+time.time(), (UDP_IP, UDP_PORT))
+
+				sendReply = RREP+data[1:]+":"+node+"#"+msg2+"#"+str(time.time())
+                                print "sendReply " ,sendReq
+                                sock.sendto(sendReply , (UDP_IP, UDP_PORT))
                                 #print "send s2" ,s2,"ip", ss
                                 addr = None
                 else:
@@ -62,8 +70,10 @@ while time.time() < t_end:
                                 #print "we have node"
                                 if node == data[2:5]:
                                         if data[2:] not in RList:
-                                                RList.append(data[2:])
-                                                print "path to ",data[-3:]," is ",data[2:],"time elaspsed ", str(time.time() - startTime) 
+						print "endtime",str(time.time()) ,"starttime",startTime
+						latency = str(time.time() - float(startTime))
+                                                RList.append(data[2:]+":latency="+latency)
+                                                print "path to ",data[-3:]," is ",data[2:],"latency ", latency 
                                 else:
                                         idx = data.find(node)
                                         s= UDP_IP[:-3]+data[idx-4:idx-1]
@@ -71,7 +81,9 @@ while time.time() < t_end:
                                         #sock.sendto(data, (UDP_IP[:-3]+data[idx-4:idx-1], UDP_PORT))
 
                                         if data[2:] not in RList:
-                                                sock.sendto(data, (UDP_IP, UDP_PORT))
+						send = data+"#"+forwardData
+						print "not in list"," data:" ,data
+                                                sock.sendto(send , (UDP_IP, UDP_PORT))
                                                 RList.append(data[2:])
                                 addr = None
 
