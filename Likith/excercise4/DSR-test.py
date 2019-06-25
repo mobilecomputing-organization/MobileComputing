56787a5ce4a271709d29b169b4d75b2dd3497da1
likhith-bp <49592284+likhith-bp@users.noreply.github.com>
2019-06-25 20:27:37 +0200

    Update DSR-test.py

--- Likith/excercise4/DSR-test.py
+++ Likith/excercise4/DSR-test.py
@@ -16,8 +16,8 @@ addr = None
 RREP="1"
 RREQ="0"
 RList=[]
-msg="msg"
-msg2="msg2"
+msg=" "
+msg2=" "
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -39,43 +39,31 @@ while time.time() < t_end:
 
     try:
         fulldata,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        fdata=fulldata.split("#",1)
-	print "fulldata: ",fulldata
-	print "fdata: ",fdata
-	data = fdata[0]
-        forwardData = fdata[1]
-	msgData = forwardData .split("#",1)
-	msg2 = msgData [0]
-        startTime = msgData [1]
+        data = fulldata.split("#",2)[0]
+        msg2 = fulldata.split("#",2)[1]
+        startTime = fulldata.split("#",2)[2]
         #data[0:123.12.12.33#hello word#125445545time stamp in ms]
         if addr is not None and addr[0] != IP:
                 print  "from: ", addr[0], " data: ", data , "msgRecv",msg2
                 if data[0] == RREQ:
                         if node not in data:
-                                sendReq = data+":"+node+"#"+forwardData
-                                print "sendReq  " ,sendReq 
-                                sock.sendto(sendReq  , (UDP_IP, UDP_PORT))
+                                s1 = data+":"+node
+                                #print "send s1 " ,s1
+                                sock.sendto(data+":"+node, (UDP_IP, UDP_PORT))
                                 #time.sleep(1)
 
                                 s2 = RREP+data[1:]+":"+node
                                 ss= UDP_IP[:-3]+data[-3:]
-
-				sendReply = RREP+data[1:]+":"+node+"#"+msg2+"#"+str(time.time())
-                                print "sendReply " ,sendReq
-                                sock.sendto(sendReply , (UDP_IP, UDP_PORT))
+                                sock.sendto(RREP+data[1:]+":"+node+"#"+time.time(), (UDP_IP, UDP_PORT))
                                 #print "send s2" ,s2,"ip", ss
                                 addr = None
                 else:
                         if node in data:
                                 #print "we have node"
                                 if node == data[2:5]:
-					saveData=data.split("#",1)[0]
-                                        if saveData[2:] not in RList:
-						print "endtime",str(time.time()) ,"starttime",startTime
-						latency = str(time.time() - float(startTime))
-                                                #RList.append(data[2:]+"#latency="+latency)
-						RList.append(saveData[2:])
-                                                print "path to ",data[-3:]," is ",saveData[2:],"latency ", latency 
+                                        if data[2:] not in RList:
+                                                RList.append(data[2:])
+                                                print "path to ",data[-3:]," is ",data[2:],"time elaspsed ", str(time.time() - startTime)
                                 else:
                                         idx = data.find(node)
                                         s= UDP_IP[:-3]+data[idx-4:idx-1]
@@ -83,9 +71,7 @@ while time.time() < t_end:
                                         #sock.sendto(data, (UDP_IP[:-3]+data[idx-4:idx-1], UDP_PORT))
 
                                         if data[2:] not in RList:
-						send = data+"#"+forwardData
-						print "not in list"," data:" ,data
-                                                sock.sendto(send , (UDP_IP, UDP_PORT))
+                                                sock.sendto(data, (UDP_IP, UDP_PORT))
                                                 RList.append(data[2:])
                                 addr = None
 
