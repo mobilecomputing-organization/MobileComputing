756533df3a6cbc83541a421414a85c5efc9c3c6a
Muddasser <50082523+muddasser27@users.noreply.github.com>
2019-06-26 01:55:14 +0200

    Latency implemented and added to remote Pi's. Final Working.
    Latency implemented and added to remote Pi's. Final Working.

--- Likith/excercise4/Flooding_backup.py
+++ Likith/excercise4/Flooding_backup.py
@@ -2,7 +2,6 @@ import socket
 import time
 import commands
 import sys
-from datetime import datetime
 
 UDP_IP = "192.168.210.255"
 IP = commands.getoutput("ifconfig wlan0 | grep -Po  'inet \K[\d.]+'")
@@ -21,21 +20,16 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind((UDP_IP, UDP_PORT))
 sock.settimeout(60)
 
-otime = str(datetime.time(datetime.utcnow()))
-otime = otime[-6:]
-
-
 if len(sys.argv) is  2:
 	orig_seqNumber = sys.argv[1]
-	sock.sendto(sys.argv[1]+otime, (UDP_IP, UDP_PORT))
+	sock.sendto(sys.argv[1], (UDP_IP, UDP_PORT))
 	
 
 while time.time() < t_end:
 
     try:
         data,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        rcvd_seqNumber = data[0]
-        Latency = 999999-int.from_bytes(date[1:],byteorder='big')
+        rcvd_seqNumber = data
         if addr is not None and addr[0] != IP:
             print  "From ", addr, "Latency", "data: ", data
             addr = None
