2413653a7e25c032568abf6941d0bcf9e8f58216
Muddasser <50082523+muddasser27@users.noreply.github.com>
2019-06-26 06:38:45 +0200

    DSR final code

--- Likith/excercise4/DSR.py
+++ Likith/excercise4/DSR.py
@@ -2,82 +2,203 @@ import socket
 import time
 import commands
 import sys
+import random
+from datetime import datetime
 
 UDP_IP = "192.168.210.255"
+
+# Routing Table - Initialise all the paths/routes to none 
+Path_Dict = {
+"192.168.210.152" : None, 
+"192.168.210.170" : None, 
+"192.168.210.173" : None,
+"192.168.210.193" : None, 
+"192.168.210.199" : None 
+}
+
+# Routing Table - Initialise all the paths/routes to none 
+SeqNo_Dict = {
+'192.168.210.152' : -1, 
+'192.168.210.170' : -1, 
+'192.168.210.173' : -1,
+'192.168.210.193' : -1, 
+'192.168.210.199' : -1 
+}
+
 IP = commands.getoutput("ifconfig wlan0 | grep -Po  'inet \K[\d.]+'")
+#IP = IP.rstrip()
 node = IP[-3:]
 UDP_PORT = 5012
-print " IP: ", IP
+print " IP:", IP
 
+# For sending data to all the nodes
+IP_List = ["192.168.210.152" , "192.168.210.170", "192.168.210.173","192.168.210.193", "192.168.210.199" ]
+    
 orig_seqNumber = -1
 rcvd_seqNumber = -1
 t_end = time.time() + 30
-addr = None
-RREP="1"
-RREQ="0"
-RList=[]
-msg=" "
-msg2=" "
+DATA = "2"
+RREP = "1"
+RREQ = "0"
+RList =[]
+msg = " "
+msg2 = " "
 
+def getRoute(orig_seqNumber, strtxtime, tmpip):
+    RREQ_Data = str(orig_seqNumber + ":" + RREQ + ":" + strtxtime + ":" + tmpip + ":" + IP + ":" )
+    print ("#################   Start initiation ###############")
+    print (str("RREQ forward: "+IP+" : "+tmpip))
+    
+    #Broadcast RREQ
+    sock.sendto(RREQ_Data, (UDP_IP, UDP_PORT))
+    
+    loopvar = False
+    routefound = True
+    route = None
+    
+    while loopvar is False:
+        try:
+            routedata, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+            routedata = routedata.split(":")
+            if routedata[1] is RREP:
+                # If the destination for RREP
+                if routedata[3] == IP:
+                    loopvar = True
+                    route = str(routedata[3]+":"+":".join(routedata[5].split("#"))+routedata[4])
+                    print(str("RREP at source, route is: "+route))
+                
+        except socket.error:
+            loopvar = True
+            routefound = False
+            print("Timeout out of getroute")
+            
+    return routefound, route; 
+    
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind((UDP_IP, UDP_PORT))
 sock.settimeout(30)
 
-if len(sys.argv) >=  2:
-	orig_seqNumber = sys.argv[1]
-	if len(sys.argv) is  3:
-                msg = sys.argv[2]
-
-        data = RREQ+":"+node+"#"+msg
-        print   " data: ", data 
-        sock.sendto(data, (UDP_IP, UDP_PORT))
-
+if len(sys.argv) is  2:
+    try:
+        IP_List.remove(IP)
+        
+    except ValueError:
+        print ("IP address not found in routing table. Exception!!!")
+        pass
+    
+    #Generate random Sequence Number
+    orig_seqNumber = str(random.randint(1,101))
+    
+    for tmpip in IP_List:
+        if Path_Dict[tmpip] is None:
+        
+            # Record time
+            txtime = str(datetime.time(datetime.utcnow()))
+            txtime = float(txtime[-9:])
+            
+            # Find Route
+            routeFlag, route = getRoute(orig_seqNumber, str(txtime), tmpip)
+            
+            rxtime = str(datetime.time(datetime.utcnow()))
+            rxtime = float(rxtime[-9:])
+            
+            latency = (rxtime - txtime) * 1000
+            
+            
+            if routeFlag is True:
+            
+                RList = route.split(":")
+                print (" Route is ", RList)
+                print (str("Time taken for route discovery is - " + str(latency) + " ms"))
+                
+                # Update path in dict
+                Path_Dict[tmpip] = route
+                
+                #Prepare DATA frame
+                data = str(orig_seqNumber + ":" + DATA + ":" + str(txtime) + ":" + tmpip + ":" + IP + ":" + Path_Dict[tmpip] + ":" + "Message sent from " + IP + ' - " ' + sys.argv[1] + ' "' + " received at the node!")
+                
+                print ("Sending Data packet")
+                #Send Data frame
+                sock.sendto(data, (UDP_IP, UDP_PORT))
+                
+            else:
+                print ("No Route found to the node") 
+            
+        else:
+                #Prepare header for flooding -> "seq_number:current_txtime"
+                data = str(orig_seqNumber + ":" + DATA +":" + str(txtime) + ":" + tmpip + ":" + IP + ":" + Path_Dict[tmpip] + ":" + "Message sent from " + IP + ' - " ' + sys.argv[1] + ' "' + " received at the node!")
+                
+                #Send Data frame
+                sock.sendto(data, (UDP_IP, UDP_PORT))       
+    
 while time.time() < t_end:
 
     try:
-        fulldata,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        data = fulldata.split("#",1)[0]
-        #msg2 = fulldata.split("#",1)[1]
-        #data[0:123.12.12.33#hello word#19:12:33time stamp]
+        rxData,addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        rxData = rxData.split(":")
         if addr is not None and addr[0] != IP:
-                print  "from: ", addr[0], " data: ", data , "msgRecv",msg2
-                if data[0] == RREQ:
-                        if node not in data:
-                                s1 = data+":"+node
-                                #print "send s1 " ,s1
-                                sock.sendto(data+":"+node, (UDP_IP, UDP_PORT))
-                                #time.sleep(1)
+            if rxData[1] == RREQ:
+                
+                # If node is not the destination node or source node and no reception of frame already received
+                if (rxData[3] != IP) and (rxData[4] != IP) and (IP not in rxData[5]):
+                    
+                    # Append node IP with a # 
+                    txData = ":".join(rxData)+IP+"#"
+                    print (str("RREQ forward: "+rxData[4]+" : "+" : ".join(txData.split(":")[5].split("#"))+rxData[3]))
+                    
+                    # Broadcast RREQ frame after append
+                    sock.sendto(txData, (UDP_IP, UDP_PORT))
+                
+                # If destination node and for first frame arrival
+                if (rxData[3] == IP):# and (SeqNo_Dict[str(rxData[4]] != int(rxData[0])):
+                    # Append last destination node IP, swap the destination IP and source IP address
+                    #print (int(rxData[0]))
+                    #print (int(SeqNo_Dict[str(rxData[4])]))
+                    
+                    SeqNo_Dict = rxData[0]
+                    rxData[1] = RREP
+                    rxData[3] = rxData[4]
+                    rxData[4] = IP
+                    
+                    #Prepare Tx frame
+                    #txData = rxData.join(":")+node
+                    txData = ":".join(rxData)
+                    print (str("RREQ Destination: "+rxData[4]+" :" +" : ".join(txData.split(":")[5].split("#"))+rxData[3]))
+                    
+                    # Broadcast RREQ frame after append
+                    sock.sendto(txData, (UDP_IP, UDP_PORT))
+                    
+            elif rxData[1] is RREP:
+                # If node is not the destination node and the current node is a part of the route
+                if rxData[3] != IP and IP in rxData[5]:
+                    print (str("RREP forward : "+rxData[4]+" : "+" : ".join(rxData[5].split("#"))+rxData[3]))
+                    # Broadcast RREP frame after append
+                    sock.sendto(":".join(rxData), (UDP_IP, UDP_PORT))
+                
+                # The function will never hit for destination node as this while loop is never entered for the same        
                                 
-                                s2 = RREP+data[1:]+":"+node
-                                ss= UDP_IP[:-3]+data[-3:]
-                                sock.sendto(RREP+data[1:]+":"+node, (UDP_IP, UDP_PORT))
-                                #print "send s2" ,s2,"ip", ss
-                                addr = None
-                else:
-                        if node in data:
-                                #print "we have node"
-                                if node == data[2:5]:
-                                        if data[2:] not in RList:
-                                                RList.append(data[2:])
-                                                print "path to ",data[-3:]," is ",data[2:]
-                                else:
-                                        idx = data.find(node)
-                                        s= UDP_IP[:-3]+data[idx-4:idx-1]
-                                        print "from",addr[0],"to: ",s ," data:" ,data
-                                        #sock.sendto(data, (UDP_IP[:-3]+data[idx-4:idx-1], UDP_PORT))
+            elif rxData[1] is DATA:
+                # If node is not the destination node and the current node is a part of the route
+                if rxData[3] != IP and IP in rxData[5]:
+                    print (str("DATA forward : "+rxData[4]+" : "+" : ".join(rxData[5].split("#"))+rxData[3]))
+                    # Broadcast DATA frame after append
+                    sock.sendto(":".join(rxData), (UDP_IP, UDP_PORT))
+                    
+                # If destination node
+                if rxData[3] == IP:
+                    print (str("Message sent from " + rxData[4] + ' - " ' + rxData[6] + ' "' + " received at the node!"))
+            
+            else:
+                print ("Incorrect frame type received. Exception!!!!!!")
 
-                                        if data[2:] not in RList:
-                                                sock.sendto(data, (UDP_IP, UDP_PORT))
-                                                RList.append(data[2:])
-                                addr = None
-
-
+        addr = None
+            
+            
     except socket.error:
+        addr = None
         pass
 
 sock.close()
-print "finished"
-print "Route list ",RList
 print "finished"
